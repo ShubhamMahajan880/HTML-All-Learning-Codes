99466e7540351ac8c57ddf6c904099646710f691
ShubhamMahajan <115464803+ShubhamMahajan880@users.noreply.github.com>
2023-05-17 17:39:40 +0530

    Add files via upload

---
+++ my HTML notes ...py
@@ -0,0 +1,101 @@
+#1) in vs code.. use ! to get html 5 structure of codes
+#2) in paragrpah tag for avoiding the horizontal paragraph and like a nrmal pessage we can use "Alt+Z" command
+#3) In image section ALT command is uses for refernce of image, that in case when image is not able to open then it show that alternative text
+#4) Pre tag in HTML is uses for getting the required space and empty lines
+#5) target  = _main uses for open the webpage in new tab
+
+#6) div:- in div tag we devide the sections sp that style can be added differently
+
+#7) "Paragraph" is a "block elemnt" while "span" is a "inline elemnt"
+
+""" "br" command is uses for start anything from a new line while "hr" tag is uses for inserting a horzontal line """
+
+""" By clicking an image it open any page in new tab is done by using ANCHOR TAG <a href = ".. target= "blank"> img src = ".." </a> Here before the anchor CLOSING tag we can use img source. """
+
+""" HENCE only th syntax is responsible for merging all columns <th colspan="5">XYZ</th> """
+
+#8) <!-- Some Block Elements(DIV) -->
+            <!-- 
+                <address> <article> <aside> <blockquote> <canvas> <dd> <div> <dl> <dt> <fieldset> <figcaption> <figure> <footer> <form> <h1> -<h6> <header> <hr><li> <main> <nav> <noscript> <ol> <p> <pre> <section> <table> <tfoot> <ul> <video>
+            -->
+            
+#9) <!-- Some Inline Elememts(SPAN) -->
+            <!-- 
+                <a> <abbr> <acronym> <b> <bdo> <big> <br> <button> <cite> <code> <dfn> <em> <i> <img> <input> <kbd> <label> <map> <object> <output> <q> <samp> <script> <select> <small> <span> <strong> <sub> <sup> <textarea> <time> <tt> <var>
+            -->
+#10) Alt + Mouse Cursor: - For selecting all the last tags
+            
+#11) In unordered List:- we can insrt any shape like :- Circle,Square
+            
+#12) in Ordered List:- we can insert symbols as :- Roman,A,a,1
+            
+#13) "th" is uses for TABLEHEADING while "td" is uses for TABLEDATA
+            
+#14) Colspan and rowspan are two commands uses for mixing multiple columns or rows into a single one
+
+#15) input type = "text" is uses for take the input from user and "placeholder" is uses for represent tha information which we have to insert in that box            
+
+#16) id tag is uses for get the difference between both the same fiels:- like shubham in a class then for identfy the 1 shubham we use id 1 which work as Enrollment no. of shubham            
+
+#17)label tag is uses for distinguish between different sections we created for distinguish
+
+#18) name of label and id both should be same for a sinle selector
+
+#19) name for all three section should be similar
+
+#20) "id" - uses for alot different different roll no to students and "class" - uses for vo studenta kon se club me h ya kon se alg alg sectrions me h
+
+#21) Radio and Checkbox both are different things :- Radio is uses for selecting either one while checkbox is uses for check or tick all the options we wants to tick
+
+#22) Textarea - command is uses for defining the reason or writing a paragraph
+
+#23) Select - command is uses for selecting any option
+
+""" After the value tag name should be same for each input type section. and id must be different for section of different cases """
+#24) In rdio button we can select any one option from given options while in CHECKBOX w can select more than one or may be all
+
+
+
+#25) in "meta:description" :we add details of our page so that better SEo can performed & "meta:keyword" also help in it by using cetain keywords we can do it.
+
+" In Radio Button for select any one from give options NAME should be same and value and id should be different
+" Fieldset is a propety uses for insrting border at any name or box creation for any option
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
